1e7fc253c052619c6e41ae6b06db5d273cd51707
IBNU HILMI <ibnu.hilmi327@guru.smp.belajar.id>
2026-06-03 21:13:10 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="p-10">
-      <h1 className="text-4xl font-bold text-blue-700">
-        School Attendance System
-      </h1>
-      <p className="mt-4 text-gray-600">
-        Sistem absensi dan nilai siswa modern.
-      </p>
+    <main className="flex items-center justify-center min-h-screen">
+      <div className="card text-center w-[400px]">
+        <h1 className="text-4xl font-bold text-blue-700 mb-4">
+          School Attendance System
+        </h1>
+
+        <p className="text-gray-500 mb-6">
+          Website Absensi dan Nilai Siswa
+        </p>
+
+        <Link
+          href="/login"
+          className="bg-blue-600 text-white px-6 py-3 rounded-lg"
+        >
+          Login Sekarang
+        </Link>
+      </div>
     </main>
   )
 }

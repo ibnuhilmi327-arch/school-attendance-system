9c7d80da54e75a2ca33b7a38cb9528b19245c4d3
IBNU HILMI <ibnu.hilmi327@guru.smp.belajar.id>
2026-06-03 21:11:34 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import "./globals.css"
 
+export const metadata = {
+  title: "School Attendance System",
+  description: "Website Absensi dan Nilai Siswa",
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="id">
       <body>
         {children}
       </body>

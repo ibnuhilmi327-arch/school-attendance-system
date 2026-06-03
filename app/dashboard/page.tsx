4f0a3cdbee8996e152393f6052f6c0a07fc50b37
IBNU HILMI <ibnu.hilmi327@guru.smp.belajar.id>
2026-06-03 21:13:51 +0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,47 @@
+import Sidebar from "@/components/Sidebar"
 
-export default function Dashboard() {
+export default function DashboardPage() {
   return (
-    <main className="p-6">
-      <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
+    <div className="flex">
+      <Sidebar />
 
-      <div className="grid md:grid-cols-3 gap-4">
-        <div className="bg-blue-500 text-white p-6 rounded-xl">
-          <h2>Total Siswa</h2>
-          <p className="text-4xl font-bold">320</p>
-        </div>
+      <main className="flex-1 p-6">
+        <h1 className="text-4xl font-bold mb-6">
+          Dashboard
+        </h1>
 
-        <div className="bg-green-500 text-white p-6 rounded-xl">
-          <h2>Kehadiran Hari Ini</h2>
-          <p className="text-4xl font-bold">290</p>
-        </div>
+        <div className="grid md:grid-cols-3 gap-5">
+          <div className="card">
+            <h2 className="text-gray-500">
+              Total Siswa
+            </h2>
 
-        <div className="bg-purple-500 text-white p-6 rounded-xl">
-          <h2>Rata-rata Nilai</h2>
-          <p className="text-4xl font-bold">88</p>
+            <p className="text-5xl font-bold text-blue-700 mt-2">
+              320
+            </p>
+          </div>
+
+          <div className="card">
+            <h2 className="text-gray-500">
+              Kehadiran Hari Ini
+            </h2>
+
+            <p className="text-5xl font-bold text-green-600 mt-2">
+              290
+            </p>
+          </div>
+
+          <div className="card">
+            <h2 className="text-gray-500">
+              Rata-rata Nilai
+            </h2>
+
+            <p className="text-5xl font-bold text-purple-600 mt-2">
+              88
+            </p>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   )
 }

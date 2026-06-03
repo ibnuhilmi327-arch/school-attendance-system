c3a811e6e6b9a8bd05156c7273c55334f382b858
IBNU HILMI <ibnu.hilmi327@guru.smp.belajar.id>
2026-06-03 21:13:30 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,37 @@
+"use client"
+
+import { useRouter } from "next/navigation"
 
 export default function LoginPage() {
+  const router = useRouter()
+
+  function handleLogin() {
+    router.push("/dashboard")
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-xl shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-6">Login Guru / Admin</h1>
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <div className="bg-white p-8 rounded-2xl shadow-xl w-[400px]">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Login Admin / Guru
+        </h1>
 
         <input
           type="email"
           placeholder="Email"
-          className="w-full border p-3 rounded mb-4"
+          className="w-full border p-3 rounded-lg mb-4"
         />
 
         <input
           type="password"
           placeholder="Password"
-          className="w-full border p-3 rounded mb-4"
+          className="w-full border p-3 rounded-lg mb-4"
         />
 
-        <button className="bg-blue-600 text-white w-full p-3 rounded">
+        <button
+          onClick={handleLogin}
+          className="bg-blue-600 text-white w-full p-3 rounded-lg"
+        >
           Login
         </button>
       </div>
